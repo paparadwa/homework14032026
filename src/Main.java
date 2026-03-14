8e455f0aea2444e4f0f8789a7bb872be7275d910
paparadwa <timoshkov_a@bk.ru>
2026-03-14 17:29:33 +0300

    hw is updated. try 2

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,14 @@ public class Main {
         short clientDeviceYear = 2015;
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+        }
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
         if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+        }
+        if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
         //task 3

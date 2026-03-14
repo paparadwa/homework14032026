ec7dc9450069568b04ff60409c097c98ba81c7e5
paparadwa <timoshkov_a@bk.ru>
2026-03-14 13:36:34 +0300

    hw is updated

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,27 @@ public class Main {
     public static void main(String[] args) {
         //task 1
         byte clientOS = 0;
-        if (clientOS == 0){
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         //task 2
         short clientDeviceYear = 2015;
-        if (clientOS == 0){
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-        } else if (clientDeviceYear >= 2015){
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
         //task 3
         short year = 2021;
-        if (year > 1584){
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+        if (year > 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
                 System.out.println("Год является високосным");
             } else {
                 System.out.println("Год не является високосным");
@@ -33,45 +32,42 @@ public class Main {
         }
         //task 4
         int deliveryDistance = 95;
-        if (deliveryDistance <= 20){
+        if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней: 1");
-        } else if (deliveryDistance <= 60){
+        } else if (deliveryDistance <= 60) {
             System.out.println("Потребуется дней: 2");
-        } else if (deliveryDistance <= 100){
+        } else if (deliveryDistance <= 100) {
             System.out.println("Потребуется дней: 3");
         } else {
             System.out.println("Доставки нет");
         }
         //task 5
         byte monthNumber = 2;
-        if (monthNumber <= 12) {
-            switch (monthNumber) {
-                case 12:
-                case 1:
-                case 2:
-                    System.out.println("Месяц принадлежит к зиме");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Месяц принадлежит к весне");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Месяц принадлежит к лету");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    System.out.println("Месяц принадлежит к осени");
-                    break;
-                default:
-                    System.out.println("Ошибка!");
-                    break;
-            }
-        } else {
-            System.out.println("Номер месяца не может быть больше 12!");
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Месяц принадлежит к зиме");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Месяц принадлежит к весне");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Месяц принадлежит к лету");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Месяц принадлежит к осени");
+                break;
+            default:
+                System.out.println("Ошибка!");
+                break;
         }
+
     }
 }
